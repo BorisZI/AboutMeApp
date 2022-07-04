1f322edbc21a4bb0e13ffbe202c55bcea2509d36
Boris Z <Boris Z>
2022-07-04 14:28:04 +0300

    Initial Commit

---
+++ AboutMeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeApp
+//
+//  Created by Boris Alexandrov on 04.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
