0cff4de083b5c8c87eca90287c88f5e059fe3bb9
Boris Z <Boris Z>
2022-07-05 16:46:21 +0300

    oneMoreCommit

--- AboutMeApp/ViewControllers/ViewController.swift
+++ AboutMeApp/ViewControllers/ViewController.swift
@@ -9,10 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Outlets
+    
+    @IBOutlet var userTextField: UITextField!
+    @IBOutlet var passTextField: UITextField!
+    
+    // MARK: - Private properties
+    //private let user = User.getUserData()
+    
+    // MARK: Navigation
+    
+    
+    
+    // MARK: IBActions
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
---
+++ AboutMeApp/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WelcomeViewController.swift
+//  AboutMeApp
+//
+//  Created by Boris Zotserman on 05.07.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
